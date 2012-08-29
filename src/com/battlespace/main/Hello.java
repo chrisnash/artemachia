02803ace626c0631eaca1033a4ea9a10b6079888
chrisnash <guestblogme@gmail.com>
2012-08-29 12:53:11 -0700

    Basic project folder setup

---
+++ src/com/battlespace/main/Hello.java
@@ -0,0 +1,15 @@
+package com.battlespace.main;
+
+public class Hello
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ test/com/battlespace/main/TestHello.java
@@ -0,0 +1,16 @@
+package com.battlespace.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHello
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
