b0cce0e18124eb1fa06faba0cfd14a08f5f7265b
chrisnash <guestblogme@gmail.com>
2012-09-08 16:50:09 -0700

    More replays added to include more player and enemy ship information, including pirates

--- src/com/battlespace/main/ReplayRunner.java
+++ src/com/battlespace/main/ReplayRunner.java
@@ -49,7 +49,7 @@ public class ReplayRunner
         EnemyShipDatabase esd = EnemyShipDatabase.load();
         
         //String replayFile = "data/replays/" + args[0];
-        String replayFile = "data/replays/" + "kavandi_1.txt";
+        String replayFile = "data/replays/" + "regor_1.txt";
         
         FileData replay = DataLoaderService.loadFile(replayFile);
 
